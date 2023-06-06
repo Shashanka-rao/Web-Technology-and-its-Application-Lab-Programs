64dcd7ffcaa62c55548f2597027394d1cc9073f3
Shashanka-rao <raobnshashank567@gmail.com>
2023-06-06 21:46:31 +0530

    All programs uploaded

---
+++ program10/Queries.sql
@@ -0,0 +1,18 @@
+create table students (
+id int auto_increment, name varchar(50), usn varchar(10),
+year int,
+college varchar(50), primary key(id)
+);
+/* Insert values to the table */
+insert into students (name, usn, year, college) values ( 'Mayur', '4SF18IS053', 2018, 'Sahyadri College'
+);
+insert into students (name, usn, year, college) values ( 'Swathi', '4SF19IS115', 2019, 'Sahyadri College'
+);
+insert into students (name, usn, year, college) values ( 'Prathiksha', '4SF18IS069', 2018, 'Sahyadri College'
+);
+insert into students (name, usn, year, college) values ( 'ABC', '4SF18IS101', 2018, 'Sahyadri College'
+);
+insert into students (name, usn, year, college) values ( 'XYZ', '4SF19IS087', 2019, 'Sahyadri College'
+);
+insert into students (name, usn, year, college) values ( 'MNO', '4SF18IS021', 2018, 'Sahyadri College'
+);
